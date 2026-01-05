a685402a9ba8f65c41ce7d27fce6bbb242931df8
arshsaini15 <arshdeepp015@gmail.com>
2026-01-05 21:12:15 +0530

    UC1

---
+++ UC1.java
@@ -0,0 +1,13 @@
+public class Uc1 {
+    static class Banner1 {
+        void display() {
+            System.out.println("OOPS");
+        }
+    }
+
+    public static void main(String[] args) {
+        Banner1 b1 = new Banner1();
+        b1.display();
+
+    }
+}
